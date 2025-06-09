bdf3008415de88d5b9534d56ffdf9e376f587048
Ryodayodayo <mitosara10@gmail.com>
2025-06-09 23:41:26 +0900

    フロントエンドとバックエンドでレポジトリを分けることにした

---
+++ src/pages/Login.tsx
@@ -0,0 +1,17 @@
+import React, { useState } from 'react';
+import Header from '../components/Header'
+import LoginForm from '../components/LoginForm'
+
+const Login = () => {
+
+    return (
+        <div>
+            <Header />
+            <LoginForm />
+        </div>
+
+    );
+};
+
+export default Login;
+
