a2d849e93fff2e4558f81b4a9f7ba2115e26ccbe
Ryodayodayo <mitosara10@gmail.com>
2025-06-15 00:35:18 +0900

    トップレベルでのawaitを許可するために、tsconfig.jsonのtargetをes2022に変更

--- src/components/TemplateList.tsx
+++ src/components/TemplateList.tsx
@@ -1,3 +1,10 @@
+import { doc, getDoc } from 'firebase/firestore';
+import { useAuthContext } from '../contexts/AuthContext'
+import { db, auth } from '../firebase'
+
+const docRef = doc(db, 'collection', 'documentId');
+const docSnap = await getDoc(docRef);
+
 const TemplateList = () => {
 
   return (
