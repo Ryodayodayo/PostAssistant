e084307b41000f07a70bc8259e6fe06deda0bec5
Ryodayodayo <mitosara10@gmail.com>
2025-06-15 01:17:58 +0900

    ユーザーの保存したテンプレートをfirestoreから読み込めるようになった

--- src/components/TemplateList.tsx
+++ src/components/TemplateList.tsx
@@ -1,15 +1,78 @@
-import { doc, getDoc } from 'firebase/firestore';
+import { useState, useEffect } from 'react';
+import { doc, getDocs, collection, query, where } from 'firebase/firestore';
 import { useAuthContext } from '../contexts/AuthContext'
 import { db, auth } from '../firebase'
 
-const docRef = doc(db, 'collection', 'documentId');
-const docSnap = await getDoc(docRef);
+
+interface Template {
+  id: string;
+  name: string;
+  content: string;
+}
 
 const TemplateList = () => {
+  const { user } = useAuthContext()
+  const [userTemplates, setUserTemplates] = useState<Template[]>([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchUserTemplates = async () => {
+      if (!user) {
+        setLoading(false);
+        return;
+      }
+
+      try {
+        setLoading(true);
+
+        const templatesRef = collection(db, 'users', user.uid, "templates");
+        const q = query(templatesRef);
+        const querySnapshot = await getDocs(q);
+      
+        const userTemplates : Template[] = [];
+
+        querySnapshot.forEach((doc) => {
+        const data = doc.data();
+
+        userTemplates.push({
+          id: doc.id,           // ドキュメントIDを取得
+          name: data.name,      // nameフィールドを取得
+          content: data.content // contentフィールドを取得
+        });
+      });
+
+      setUserTemplates(userTemplates);
+      console.log('取得したテンプレート:', userTemplates);
+
+      } catch (err) {
+        console.error('テンプレートの取得に失敗しました:', err);
+      } finally{
+        setLoading(false);
+      }
+    }
+ 
+    fetchUserTemplates();
+  }, [user]);
 
   return (
     <div>
       <h2>テンプレート一覧</h2>
+
+      {loading && <p>読み込み中...</p>}
+      
+      {!loading && userTemplates.length === 0 && (
+        <p>テンプレートがありません。</p>
+      )}
+      
+      {!loading && userTemplates.length > 0 && (
+        <ul>
+          {userTemplates.map((template) => (
+            <li key={template.id}>
+              {template.name}
+            </li>
+          ))}
+        </ul>
+      )}
        
     </div>
   );
